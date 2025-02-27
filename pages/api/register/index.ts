136fbcdab6a935c01e863f474cd14c97cd24ac2d
Deepanshu <deepanshusaini502@gmail.com>
2025-02-27 09:42:31 +0530

    Fixed Password Encryption

--- pages/api/register/index.ts
+++ pages/api/register/index.ts
@@ -1,23 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../helper/api/db";
+import bcrypt from "bcryptjs";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const {
+    let {
       name,
       email,
       password,
-    }: { name: string; email: string; password: string } =JSON.parse(req.body);
-    console.log(name,email);
+      referral_code,
+    }: {
+      name: string;
+      email: string;
+      password: string;
+      referral_code: string;
+    } = JSON.parse(req.body);
+    password = bcrypt.hashSync(password, 11);;
+    referral_code = password.split(".")[1];
     const data = await prisma.users.create({
-      data: { name, email, password, referral_code: "testing" },
+      data: { name, email, password, referral_code },
     });
 
+   
     res.status(200).json(data);
+    
   } catch (error) {
-   console.log(error)
+    console.log(error);
   }
 }
